17bc0df2f6808f7ddf668f78dcce8309158fa27a
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-09-13 12:25:08 +0100

    added loops to determine x and os

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ const TicTacToe = (() => {
 		'use strict';
 		let board = ["", "", "", "", "", "", "", "", ""];
 		let turn = "";
+
+		function updateBoard() {
+			for (let i = 0; i < 9; i++) {
+				let gameSquare = document.getElementById(i)
+				gameSquare.innerHTML = board[i];
+				if (board[i] == "X") {
+					gameSquare.classList.add("crosses");
+					gameSquare.classList.remove("naughts");
+				}
+				if (board[i] == "X") {
+					gameSquare.classList.add("naughts");
+					gameSquare.classList.remove("crosses");
+				}
+			}
+		}
 	})
 })
