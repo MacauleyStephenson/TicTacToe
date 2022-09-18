6b9cd2ecb0c4f435aff9780a39cabca310819af6
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-09-18 10:25:52 +0100

    renamed variables, completed script, sorting out any remaining errors

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ const TicTacToe = (() => {
 		const winningTiles = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [2, 5, 8]];
 		let count = 0;
 
-		let model = document.getElementById("myModal");
+		let model = document.getElementById("myModel");
 		let span = document.getElementsByClassName("close")[0];
 		let restartButton = document.getElementById("restart");
 		let resetButton = document.getElementById("resetButton");
@@ -96,7 +96,7 @@ const TicTacToe = (() => {
 
 		window.onclick = function (event) {
 			if (event.target == model) {
-				modal.style.display = "none";
+				model.style.display = "none";
 			}
 		}
 
@@ -118,6 +118,112 @@ const TicTacToe = (() => {
 			}
 			model.style.display = "none";
 			updateNames();
-		})
-	})
-})
+		});
+
+		// Add event listner for restart button after game won
+		restartButton.addEventListener("click", function () {
+			resetBoard();
+		}); // same for reset button at bottom
+		resetButton.addEventListener("click", function () {
+			resetBoard();
+		});
+
+		player1.setTurn(true);
+		gameBoard.setPlayerTurn(player1.getcharacterSymbol());
+
+		// Add names to the board
+		function updateNames() {
+			let player1Div = document.getElementById("player1Name");
+			player1Div.innerHTML = player1.getname();
+			let player2Div = document.getElementById("player2Name");
+			player2Div.innerHTML = player2.getname();
+		}
+
+		for (let i = 0; i < 9; i++) {
+			let gameSquare = document.getElementById(i);
+			gameSquare.addEventListener("click", function () {
+				// check to see if square is empty before adding
+				let theBoard = gameBoard.getBoard();
+				if (theBoard[i] == "" || theBoard[i] == null) {
+					gameBoard.addToBoard(i);
+					checkWin();
+					count++;
+					changeTurn();
+				}
+			});
+		}
+
+		function changeTurn() {
+			if (player1.getTurn()) {
+				player2.setTurn(true);
+				player1.setTurn(false);
+				let player2Symbol = player2.getcharacterSymbol();
+				gameBoard.setPlayerTurn(player2Symbol);
+
+				// change class
+				let player2NameDiv = document.getElementById("player2Name");
+				player2NameDiv.classList.add("playerTurn");
+				let player1NameDiv = document.getElementById("player1Name");
+				player1NameDiv.classList.remove("playerTurn");
+			} else {
+				player2.setTurn(false);
+				player1.setTurn(true);
+				let player1Symbol = player1.getcharacterSymbol();
+				gameBoard.setPlayerTurn(player1Symbol);
+
+				// change class
+				let player1NameDiv = document.getElementById("player1Name");
+				player1NameDiv.classList.add("playerTurn");
+				let player2NameDiv = document.getElementById("player2Name");
+				player2NameDiv.classList.remove("playerTurn");
+			}
+		}
+		function checkWin() {
+			// Check if anyone has won
+			if (count >= 3 && count < 9) {
+				for (let i = 0; i < winningTiles.length; i++) {
+					let board = gameBoard.getBoard();
+					console.log("board: " + board);
+					console.log("count: " + count);
+					console.log("i: " + i);
+					console.log("checking combination: " + "[" + winningTiles[i][0] + "][" + winningTiles[i][1] + "]["
+						+ winningTiles[i][2] + "]")
+					if (board[winningTiles[i][0]] == player1.getcharacterSymbol() &&
+						board[winningTiles[i][1]] == player1.getcharacterSymbol() &&
+						board[winningTiles[i][2]] == player1.getcharacterSymbol()) {
+						winnerModel("The winner is: " + player1.getname());
+						break;
+					}
+					if (board[winningTiles[i][0]] == player2.getcharacterSymbol() &&
+						board[winningTiles[i][1]] == player2.getcharacterSymbol() &&
+						board[winningTiles[i][2]] == player2.getcharacterSymbol()) {
+						winnerModel("The winner is: " + player2.getname());
+						break;
+					}// check for draw (number of goes aka count reaches max and no winner then it's a draw)
+
+					else if (count == 8 && i == (winningTiles.length - 1)) {
+						winnerModel("It's a draw!");
+					}
+				}
+			}
+		}
+		function winnerModel(winner) {
+			let winnerModel = document.getElementById("winnerMod");
+			winnerModel.style.display = "block";
+			let winnerDIV = document.getElementById("winnerDIV");
+			winnerDIV.innerHTML = winner;
+		}
+
+		function resetBoard() {
+			count = 0;
+			gameBoard.resetBoard();
+			let winnerModel = document.getElementById("winnerMod");
+			winnerModel.style.display = "none";
+		}
+
+
+
+	})();
+
+
+})();
